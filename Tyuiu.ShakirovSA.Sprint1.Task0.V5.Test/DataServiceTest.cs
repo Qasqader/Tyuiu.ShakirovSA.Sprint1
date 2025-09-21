163c933a06be442d871a926820ec07a99cc7ddc9
Сергей Шакиров <sergej.shakirov.04@mail.ru>
2025-09-21 17:35:01 +0500

    Task0

--- Tyuiu.ShakirovSA.Sprint1.Task0.V5.Test/DataServiceTest.cs
+++ Tyuiu.ShakirovSA.Sprint1.Task0.V5.Test/DataServiceTest.cs
@@ -1,15 +1,15 @@
-﻿using Tyuiu.ShakirovSA.Sprint1.Task0.V0.Lib;
-namespace Tyuiu.ShakirovSA.Sprint1.Task0.V0.Test
+﻿using Tyuiu.ShakirovSA.Sprint1.Task0.V5.Lib;
+namespace Tyuiu.ShakirovSA.Sprint1.Task0.V5.Test
 {
     [TestClass]
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void ValidExpression()
+        public void ValidCalc()
         {
             DataService ds = new DataService();
             var res = ds.Calculate();
-            Assert.AreEqual(2, res);
+            Assert.AreEqual(12, res);
         }
     }
 }
--- Tyuiu.ShakirovSA.Sprint1.Task0.V5/Program.cs
+++ Tyuiu.ShakirovSA.Sprint1.Task0.V5/Program.cs
@@ -1,4 +1,4 @@
-﻿using Tyuiu.ShakirovSA.Sprint1.Task0.V0.Lib;
+﻿using Tyuiu.ShakirovSA.Sprint1.Task0.V5.Lib;
 internal class Program
 {
     private static void Main(string[] args)
@@ -7,19 +7,19 @@ internal class Program
         Console.Title = "Спринт #1 | Выполнил: Шакиров С. А. | ИБКСб-25-1";
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* Спринт #1                                                               *");
-        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
         Console.WriteLine("* Задание #0                                                              *");
-        Console.WriteLine("* Вариант #0                                                              *");
+        Console.WriteLine("* Вариант #5                                                              *");
         Console.WriteLine("* Выполнил: Шакиров Сергей Александрович | ИБКСб-25-1                     *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* УСЛОВИЕ:                                                                *");
-        Console.WriteLine("* Написать программу, которая вычисляет выражение 10 / (2 + 3)            *");
-        Console.WriteLine("* и печатает результат на экране.                                         *");
+        Console.WriteLine("* Написать консольную программу на C#, которая суммирует значения двух    *");
+        Console.WriteLine("* одинаковых массивов по длинне.                                          *");
         Console.WriteLine("*                                                                         *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
-        Console.WriteLine("* 10 / (2 + 3)                                                            *");
+        Console.WriteLine("* (1+2)*(1+9/3)                                                           *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
