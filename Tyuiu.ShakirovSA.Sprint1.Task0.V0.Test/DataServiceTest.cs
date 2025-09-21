36c9bfb4d445752d3c77a559d41b167b6b7f2a72
Сергей Шакиров <sergej.shakirov.04@mail.ru>
2025-09-21 16:53:03 +0500

    Add project files.

---
+++ Tyuiu.ShakirovSA.Sprint1.Task0.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.ShakirovSA.Sprint1.Task0.V0.Lib;
+namespace Tyuiu.ShakirovSA.Sprint1.Task0.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
