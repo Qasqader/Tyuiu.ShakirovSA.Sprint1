e71b128fab27f71b7a7890d242f9019dae7c532c
Qasqader <Tyuiu.ShakirovSA@outlook.com>
2025-09-27 17:09:53 +0300

    + Task2 V13

---
+++ Tyuiu.ShakirovSA.Sprint1.Task2.V13.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.ShakirovSA.Sprint1.Task2.V13.Lib;
+namespace Tyuiu.ShakirovSA.Sprint1.Task2.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckConverMilesToKM()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(8.05, ds.ConvertMilesToKm(5));
+        }
+    }
+}
---
+++ Tyuiu.ShakirovSA.Sprint1.Task2.V13/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ShakirovSA.Sprint1.Task2.V13.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int x;
+        Console.WriteLine("Введите значение X: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Результат:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" X милей  в км = " + ds.ConvertMilesToKm(x));
+        Console.ReadLine();
+    }
+}
