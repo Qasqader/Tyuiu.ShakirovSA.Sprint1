c9ac5072c4d0185fdb996052e6d9d3d32c665d89
Сергей Шакиров <sergej.shakirov.04@mail.ru>
2025-09-28 21:58:26 +0500

    + Task4 V5

---
+++ Tyuiu.ShakirovSA.Sprint1.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.ShakirovSA.Sprint1.Task4.V5.Lib;
+namespace Tyuiu.ShakirovSA.Sprint1.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculateValid()
+        {
+            DataService ds = new DataService();
+            double x = Math.Round((1 / Math.Abs(2 + Math.Sqrt(4))), 3); ;
+            Assert.AreEqual(x, ds.Calculate(2, 4));
+        }
+    }
+}
